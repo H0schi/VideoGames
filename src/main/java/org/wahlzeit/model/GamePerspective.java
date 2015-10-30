c9b13db56d0d0541098e13ff3c4d9d0629313b1d
H0schi <h0schiSchw@gmail.com>
2015-10-30 15:11:33 +0100

    extended Photo class and added enums

---
+++ src/main/java/org/wahlzeit/model/GamePerspective.java
@@ -0,0 +1,33 @@
+/**
+ * GamePerspective
+ * 
+ * version 0.1
+ * 
+ * date 30.10.2015
+ * 
+ * Copyright (c) by Thorsten Schwachhofer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.wahlzeit.model;
+
+/**
+ * GamePerspective describes the graphical perspective from the viewpoint of the player
+ */
+public enum GamePerspective {
+	// naming convention, see: http://stackoverflow.com/a/9712996
+	SIDESCROLLER, FIRST_PERSON, THIRD_PERSON
+}
---
+++ src/main/java/org/wahlzeit/model/VideoGamesPhoto.java
@@ -0,0 +1,103 @@
+/**
+ * VideoGamesPhoto
+ * 
+ * version 0.1
+ * 
+ * date 30.10.2015
+ * 
+ * Copyright (c) by Thorsten Schwachhofer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package org.wahlzeit.model;
+
+/**
+ * VideoGamesPhoto is a special domain inheriting the Photo class
+ */
+public class VideoGamesPhoto extends Photo {
+
+	private int releaseYear;
+	
+	private GameGenre genre;
+	private GamePerspective perspective; 
+	
+	/**
+	 * @methodtype constructor
+	 */
+//	public VideoGamesPhoto() {
+//		super(); //not necessary, see: http://stackoverflow.com/a/6318640
+//	}
+	
+	/**
+	 * @methodtype constructor
+	 */
+	public VideoGamesPhoto(PhotoId MyId) {
+		super(MyId);
+	}
+	
+	/**
+	 * @methodtype constructor
+	 */
+	public VideoGamesPhoto(int releaseYear, PhotoId MyId, GameGenre genre, GamePerspective perspective) {
+		super(MyId);
+		this.releaseYear = releaseYear;
+		this.genre = genre;
+		this.perspective = perspective;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public int getReleaseYear() {
+		return this.releaseYear;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public GameGenre getGameGenre() {
+		return this.genre;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public GamePerspective getGamePerspective() {
+		return this.perspective;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setGameGenre(GameGenre genre) {
+		this.genre = genre;
+	}
+	
+	/**
+	 * @methodtype set
+	 */
+	public void setGamePerspective(GamePerspective perspective) {
+		this.perspective = perspective;
+	}
+	
+}
